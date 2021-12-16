bfbd09b0513387a041a1a264ffbd771df6e1a12b
Nikita Fediuchin <cfnptr@gmail.com>
2021-12-16 10:51:01 +0200

    Improve scripts

--- scripts/gen-self-sign-cert.bat
+++ scripts/gen-self-sign-cert.bat
@@ -1,3 +1,14 @@
-ECHO OFF
+@ECHO OFF
+
+openssl version
+
+IF NOT %ERRORLEVEL% == 0 (
+    ECHO Failed to get OpenSSL version, please check if it's installed.
+)
+
+ECHO(
+ECHO Generating self signed certificate...
+
 openssl req -x509 -nodes -newkey rsa:2048 -sha256 -keyout private-key.pem -out certificate.pem -days 365 -subj "/CN=localhost"
+
 PAUSE
--- scripts/gen-self-sign-cert.sh
+++ scripts/gen-self-sign-cert.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+cd $(dirname "$BASH_SOURCE")
+
+if ! openssl version ; then
+    echo "Failed to get OpenSSL version, please check if it's installed."
+fi
+
+echo ""
+echo "Generating self signed certificate..."
+
 openssl req -x509 -nodes -newkey rsa:2048 -sha256 -keyout private-key.pem -out certificate.pem -days 365 -subj "/CN=localhost"
